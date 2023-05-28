9252174ec644163115927b1b7fa3da6a2776470e
James Gardener <47278538+JGardener@users.noreply.github.com>
2023-05-28 19:45:39 +0100

    api-call - remover Promise.resolve
    Removed unnecessary Promise.resolve function in fetchFunctions.ts

--- src/utils/fetchFunctions.ts
+++ src/utils/fetchFunctions.ts
@@ -3,5 +3,5 @@ export const fetchSingleCharacter = async (queryString: string) => {
   const fetchURL = `https://pokeapi.co/api/v2/pokemon/${queryString}/`;
   const response = await fetch(fetchURL);
   const data = await response.json();
-  return Promise.resolve(data);
+  return data;
 };
